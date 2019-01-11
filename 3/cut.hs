29ccb717c995f918fb48f3b589bb12af68c957b7
Rachel K <raech.kanati@gmail.com>
2019-01-11 01:21:30 +0000

    Day 3 Part 2

--- 3/cut.hs
+++ 3/cut.hs
@@ -1,9 +1,10 @@
 
 module Main where
 
-import Data.List (groupBy)
+import Data.List (groupBy, find)
 import Data.Char (isDigit)
 import qualified Data.Vector.Unboxed         as V
+import           Data.Vector.Unboxed ((!))
 import qualified Data.Vector.Unboxed.Mutable as VM
 import Data.Traversable (for)
 
@@ -11,21 +12,9 @@ data Rect = Rect
   { x0, y0, x1, y1 :: !Int }
   deriving Show
 
-width  r = x1 r - x0 r
-height r = y1 r - y0 r
-area r = width r * height r
-
 data Claim = Claim
   { cid :: !Int, crect :: !Rect }
   deriving Show
-
-intersection :: Rect -> Rect -> Maybe Rect
-intersection (Rect rx0 ry0 rx1 ry1) (Rect sx0 sy0 sx1 sy1)
-  = if rx0 < sx1 && rx1 > sx0 && ry0 < sy1 && ry1 > sy0
-      then Just rect
-      else Nothing
-  where
-    rect = Rect (max rx0 sx0) (max ry0 sy0) (min rx1 sx1) (min ry1 sy1)
 
 parse :: String -> Claim
 parse s = Claim cid (Rect rx0 ry0 rx1 ry1) where
@@ -43,20 +32,27 @@ main = do
       gridH = maximum $ fmap (y1 . crect) claims
   putStrLn $ "Bounds: " ++ show gridW ++ ", " ++ show gridH
 
-  let rects = fmap crect claims
+  let gridIndices (Rect rx0 ry0 rx1 ry1)
+        = [ y*gridW + x | x <- [rx0 .. rx1-1], y <- [ry0 .. ry1-1] ]
+
+      rects = fmap crect claims
       grid :: V.Vector Int
       grid = V.create $ do
         -- grid full of zeros
         grid <- VM.replicate (gridW * gridH) (0 :: Int)
 
         -- for each claim, add one to each cell covered by the claim
-        for rects $ \(Rect rx0 ry0 rx1 ry1) -> do
-          let pairs = (,) <$> [rx0 .. rx1 - 1] <*> [ry0 .. ry1 - 1]
-              idxs = fmap (\(x, y) -> y * gridW + x) pairs
-          for idxs $ VM.modify grid (+ 1)
+        for rects $ \r -> for (gridIndices r) (VM.modify grid (+ 1))
+
         pure grid
 
   -- count cells >= 2
-  let nLapped = V.foldl (\n c -> if c >= 2 then n + 1 else n) 0 grid
-  putStrLn $ "Part 1: " ++ show nLapped
+  let nLapped = V.foldr' (\c -> if c >= 2 then (+ 1) else id) 0 grid
+  putStrLn $ "Part 1: " ++ show nLapped ++ " sq in"
+
+  -- find nonlapping claim
+  let nonLapping c = all (== 1) cells where
+        cells = fmap (grid !) (gridIndices (crect c))
+      Just (Claim resultCid _) = find nonLapping claims
+  putStrLn $ "Part 2: #" ++ show resultCid
 
